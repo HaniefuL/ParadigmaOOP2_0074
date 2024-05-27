14dc2e99216ccc1311518a90505610be9c3dd5d2
HaniefuL <haniefulgeofarry@gmail.com>
2024-05-27 15:57:23 +0700

    menambhkan return 0;

--- Polymorphy.cpp
+++ Polymorphy.cpp
@@ -1,2 +1,28 @@
 #include <iostream>
 using namespace std;
+
+class seseorang{
+public:
+
+};
+
+class lia :public seseorang{
+public:
+    void pesan(){
+        cout << "Pesan dari lia" << endl;
+    }
+};
+
+int main(){
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    //a.seseorang::pesan();
+
+    return 0;
+}
